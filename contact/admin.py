0848cf222c51c7da117be6986f51be22e5cf6f04
ZxHenriqueZx <henriquetargaryen123@gmail.com>
2023-12-11 14:09:38 -0300

    Customizando as opções admin.ModelAdmin

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,8 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    ordering = ('-id',)
+    #list_filter = ('created_date',)
+    search_fields = ('id', 'first_name', 'last_name',)
+    list_per_page = 15 
